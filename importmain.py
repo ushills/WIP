b5659b2261c2c25b9f31edab5012a1f465aa414d
ushills <web@ushills.co.uk>
2017-02-03 10:00:36 +0000

    Added all regions to import and print

--- importmain.py
+++ importmain.py
@@ -14,3 +14,24 @@ print('Importing London data')
 importWIPdata(
     './london/londonwipdata.sqlite',
     'H:/Current Month Wips/1. London & South East')
+
+# import North East data
+print('Importing North East data')
+importWIPdata(
+    './northeast/northeastwipdata.sqlite',
+    'H:/Current Months Wips/3. North East'
+)
+
+# import North West data
+print('Importing North West data')
+importWIPdata(
+    './northwest/northwestwipdata.sqlite',
+    'H:/Current Months Wips/3b. North West'
+)
+
+# import South West data
+print('Importing South West data')
+importWIPdata(
+    './southwest/southwestwipdata.sqlite',
+    'H:/Current Months Wips/4. South West & Wales'
+)
--- printmain.py
+++ printmain.py
@@ -10,3 +10,15 @@ printgraphs('./midlands/midlandswipdata.sqlite', './midlands/graphs/')
 # print graphs for London
 print('Printing London graphs')
 printgraphs('./london/londonwipdata.sqlite', './london/graphs/')
+
+# print graphs for North East
+print('Printing North East graphs')
+printgraphs('./northeast/northeastwipdata.sqlite', './northeast/graphs/')
+
+# print graphs for North West
+print('Printing North West graphs')
+printgraphs('./northwest/northwestwipdata.sqlite', './northwest/graphs/')
+
+# print graphs for South West
+print('Printing South West graphs')
+printgraphs('./southwest/southwestwipdata.sqlite', './southwest/graphs/')
