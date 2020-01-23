ee154157882a44d9e170e8a5fc799c8fa38cf6f0
ushills <web@ushills.co.uk>
2020-01-23 10:36:13 +0000

    Added copying files to WIP drive

---
+++ copytowipdrive.py
@@ -0,0 +1,8 @@
+import shutil
+
+shutil.copytree("./london/graphs/", "H:/Performance Graphs/london", dirs_exist_ok=True )
+shutil.copytree("./midlands/graphs/", "H:/Performance Graphs/midlands", dirs_exist_ok=True )
+shutil.copytree("./northeast/graphs/", "H:/Performance Graphs/northeast", dirs_exist_ok=True )
+shutil.copytree("./northwest/graphs/", "H:/Performance Graphs/northwest", dirs_exist_ok=True )
+shutil.copytree("./southwest/graphs/", "H:/Performance Graphs/southwest", dirs_exist_ok=True )
+print("Copying files complete!")
